10f955aff1c741ac0d420e59fdb2a183ccce468a
Joe1220 <joe1220@daum.net>
2019-09-13 17:34:19 +0900

    [env: data] - [add] resume.json 파일 하나로 전체 페이지 관리

--- src/components/organisms/pages/Home/index.tsx
+++ src/components/organisms/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { H1, Hr, Link } from "components/atoms"
 import { DownArrow } from "components/atoms/Svg"
 import { IconNav } from "components/molecules"
 import Theme from "config/styles"
+import JSON from "config/resume.json"
 
 const Home: React.SFC = () => {
   return (
@@ -17,7 +18,7 @@ const Home: React.SFC = () => {
       }
     >
       <H1 thick white>
-        Jo Seung Hyun
+        {JSON.basics.name}
       </H1>
       <Hr />
       <IconNav color={Theme.colors.white} />
